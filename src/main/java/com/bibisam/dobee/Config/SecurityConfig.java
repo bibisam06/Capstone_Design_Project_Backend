089c674830c160f13bb2271944f53cdd54c1c716
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2024-11-07 19:31:59 +0900

    keystore생성

--- src/main/java/com/bibisam/dobee/Config/SecurityConfig.java
+++ src/main/java/com/bibisam/dobee/Config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -25,16 +23,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http// CSRF 보호 비활성화
-                .authorizeHttpRequests(authorizeRequests ->
+        http
+                .csrf(csrfConfigurer -> csrfConfigurer.disable())  // CSRF 보호 비활성화
+                .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers("/api/user/**", "/api/vote/**", "/api/association/**").permitAll()
                                 .anyRequest().authenticated())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) // 필요 시 세션 생성
-                .requiresChannel(channel ->
-                        channel.anyRequest().requiresSecure()) // HTTPS 요청만 허용
-                .httpBasic(withDefaults());
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // 필요 시 세션 생성
+                );
+
 
         return http.build();
     }
