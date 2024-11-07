3e99e310e72125053b58420bc23ad178da193130
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2024-11-07 15:42:26 +0900

    https로변경
    테스트

--- src/main/java/com/bibisam/dobee/Config/SecurityConfig.java
+++ src/main/java/com/bibisam/dobee/Config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,16 +25,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrfConfigurer -> csrfConfigurer.disable())  // CSRF 보호 비활성화
-                .authorizeHttpRequests((authorizeRequests) ->
+        http// CSRF 보호 비활성화
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/user/**", "/api/vote/**", "/api/association/**").permitAll()
                                 .anyRequest().authenticated())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // 필요 시 세션 생성
-                );
-
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) // 필요 시 세션 생성
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure()) // HTTPS 요청만 허용
+                .httpBasic(withDefaults());
 
         return http.build();
     }
