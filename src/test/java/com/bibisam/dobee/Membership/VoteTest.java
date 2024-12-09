c2a4ec317233b117d732560dd00dc01c12c78621
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2024-12-09 23:50:58 +0900

    테스트

--- src/test/java/com/bibisam/dobee/Membership/VoteTest.java
+++ src/test/java/com/bibisam/dobee/Membership/VoteTest.java
@@ -1,7 +1,32 @@
 package com.bibisam.dobee.Membership;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@Transactional
 @SpringBootTest
 public class VoteTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("투표 생성 api 테스트")
+    public void vote_test(){
+
+    }
 }
